2431dff35e72df124cfb28fb9629fe35e1356431
amargura-evil-hehehe <100429271@alumnos.uc3m.es>
2022-03-16 18:39:15 +0100

    a

--- js/ajustes.js
+++ js/ajustes.js
@@ -1,8 +1,7 @@
-var canvas = document.getElementById("micanvas"); //invocar a mi canvas
-var ctx = canvas.getContext("2d");
-var x,y,dx, dy;
-var downPressed= false;
-var upPressed= false;
-var raqy= 30;
+var canvas1 = document.getElementById("ajustes_audio"); //invocar a mi canvas
+var canvas2 = document.getElementById("personalizacion");
+var ctx = canvas1.getContext("2d");
+var ctx = canvas2.getContext("2d");
+var pressed= true;
 
 window.onload=init;
