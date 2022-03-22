b15d2dbb906e3d78e49b660622a7db89c1d8e184
amargura-evil-hehehe <100429271@alumnos.uc3m.es>
2022-03-23 00:33:30 +0100

    a

--- js/ajustes.js
+++ js/ajustes.js
@@ -3,10 +3,12 @@ var canvas2 = document.getElementById("personalizacion");
 var ctx1 = canvas1.getContext("2d");
 var ctx2 = canvas2.getContext("2d");
 var pressed= true;
-const boton_mas= document.getElementById("mas");
-const boton_menos= document.getElementById("menos");
+const boton= document.getElementById("boton");
 let sonido1 = new Audio();
-sonido1.src="/audio/click/39562__the-bizniss__mouse-click.wav";
+sonido1.src="../audio/click/39562__the-bizniss__mouse-click.wav";
+
+
+
 
 window.onload=init;
 
@@ -14,12 +16,16 @@ var icono_volumen = new Image();
 
 
 
-function init(){     
+
+function init(){  
+    boton.addEventListener('click',function(){
+        sonido1.play();
+    });   
    pintarIconos();
 }
 
 function pintarIconos(){
-    icono_volumen.src="/images/pixil-frame-0.png";
+    icono_volumen.src="../images/pixil-frame-0.png";
     ctx1.drawImage(icono_volumen,600,600);
 }
 
