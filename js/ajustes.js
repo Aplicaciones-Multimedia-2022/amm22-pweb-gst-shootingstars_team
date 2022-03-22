ed61b16cdcd141012d0aa3dd02f3b5c85bf27683
amargura-evil-hehehe <100429271@alumnos.uc3m.es>
2022-03-22 19:31:17 +0100

    a

--- js/ajustes.js
+++ js/ajustes.js
@@ -1,6 +1,6 @@
-//var canvas1 = document.getElementById("ajustes_audio"); //invocar a mi canvas
+var canvas1 = document.getElementById("ajustes_audio"); //invocar a mi canvas
 var canvas2 = document.getElementById("personalizacion");
-//var ctx1 = canvas1.getContext("2d");
+var ctx1 = canvas1.getContext("2d");
 var ctx2 = canvas2.getContext("2d");
 var pressed= true;
 
@@ -13,20 +13,17 @@ window.onload=init;
 
 
 function init(){     
-   
-        var nave1=new Image();
-        nave1.src="/images/Ship1.png";
-        nave1.onload=function(){
-            ctx2.drawImage(nave1,0, 0);
-        }
+    var img1 = document.getElementById("scream");
+    ctx1.drawImage(img1, 10, 10);
     
-}
+};
+
 
 
 function aumentarVolumen(){
 
-}
+};
 
 function elegirNave(){
 
-}
+};
