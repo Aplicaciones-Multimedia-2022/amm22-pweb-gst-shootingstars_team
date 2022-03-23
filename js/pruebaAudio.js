bfa1ad0c3b6513050dbda464cf332a96ea300404
amargura-evil-hehehe <100429271@alumnos.uc3m.es>
2022-03-23 16:37:36 +0100

    a

--- js/pruebaAudio.js
+++ js/pruebaAudio.js
@@ -1,2 +1,26 @@
-const audio = new Audio("C:\Users\jaeja\Desktop\CURSO 2021-22\segundo cuatri 2122\aplicaciones multimedia\amm22-pweb-gst-shootingstars_team-1\audio\cancion2.mp3");
-audio.play();
+
+var canvas= document.getElementById("putafoto");
+var ctx= canvas.getContext("2d");
+window.onload = init;
+var imagen= new Image();
+
+
+
+function init(){
+pintar();
+bola();
+}
+
+function pintar(){
+    imagen.src="../images/Ship1.png";
+    ctx.drawImage(imagen,0,0);
+}
+
+function bola(){
+  ctx.beginPath();
+  ctx.arc(0, 0, 10, 0, 2 * Math.PI);
+  ctx.fillStyle = "white";
+  ctx.fill();
+  ctx.closePath();
+}
+
