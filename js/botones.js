88b580335984dbb5a37c0f590894ac82e9b9e4a3
amargura-evil-hehehe <100429271@alumnos.uc3m.es>
2022-04-04 13:04:39 +0200

    a

--- js/botones.js
+++ js/botones.js
@@ -3,9 +3,10 @@ const boton2= document.getElementById("boton2");
 const boton3= document.getElementById("boton3");
 const boton4= document.getElementById("boton4");
 const boton5= document.getElementById("boton5");
-const boton6= document.getElementById("boton6");
-const boton7= document.getElementById("boton7");
+const boton6= document.getElementById("bajustes");
+const boton7= document.getElementById("bjuego");
 let sonido1 = new Audio();
+let espera;
 sonido1.src="../audio/click/39562__the-bizniss__mouse-click.wav";
 
 window.onload=init;
@@ -17,15 +18,71 @@ window.onload=init;
 function init(){  
     boton1.addEventListener('click',function(){
         sonido1.play();
+        espera = setTimeout(redireccionar1, 400);
+
     }); 
     boton2.addEventListener('click',function(){
       sonido1.play();
+      espera = setTimeout(redireccionar2, 400);
     }); 
     boton3.addEventListener('click',function(){
       sonido1.play();
+      espera = setTimeout(redireccionar3, 400);
     }); 
     boton4.addEventListener('click',function(){
       sonido1.play();
+      espera = setTimeout(redireccionar4, 400);
     }); 
+    boton5.addEventListener('click',function(){
+      sonido1.play();
+      espera = setTimeout(redireccionar5, 400);
+    });
+    boton6.addEventListener('click',function(){
+      sonido1.play();
+      espera = setTimeout(redireccionar6, 400);
+    }); 
+    boton7.addEventListener('click',function(){
+      sonido1.play();
+      espera = setTimeout(redireccionar7, 400);
+    });
 
 }
+
+
+function redireccionar1(){
+  location.replace("../html/ComoJugar.html")
+}
+
+function redireccionar2(){
+  location.replace("../html/autores.html")
+}
+
+function redireccionar3(){
+  location.replace("../html/multimedia.html")
+}
+
+function redireccionar4(){
+  location.replace("../html/desarrollo.html")
+}
+
+function redireccionar5(){
+  location.replace("../html/referencias.html")
+}
+
+function redireccionar6(){
+  location.replace("../html/ajustes.html")
+}
+
+function redireccionar7(){
+  location.replace("../html/juego.html")
+}
+
+
+
+
+
+
+/*opciones de como hacerlo 
+hacerlo por element by class para no tener que hacerlo para cada boton
+luego para que haga el sonido antes de cambiarse de pagina hacer el enlace en javascripr
+con un pequeño retardo antes de irse al destino y que suene el sonidito*/
