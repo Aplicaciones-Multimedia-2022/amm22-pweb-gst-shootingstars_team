cab010cffa3d4115b31ba85605ffaec6f97afc5b
ire <100293388+irenetrrs@users.noreply.github.com>
2022-04-05 13:20:13 +0200

    ire

--- js/prejuego.js
+++ js/prejuego.js
@@ -1,52 +1,26 @@
 var mycanvas = document.getElementById("div_izq");
 var ctx = mycanvas.getContext("2d");
 
-
-
-
 var Meteorito1 = new Image();
-
 
 var metX_izq = 0;
 var metY = 800;
 var dy = -2;
 
-
-
-
-
-    
-   
-    
-
-
-
-
-function pintar_cosa_izq(){
-    
-    Meteorito1.src = "../images/grogu.png";
-   ctx.drawImage(Meteorito1, metX_izq, metY);
-    
+function pintar_cosa_izq() {
+  Meteorito1.src = "../images/grogu.png";
+  ctx.drawImage(Meteorito1, metX_izq, metY);
 }
 
+function draw_izq() {
+  ctx.clearRect(0, 0, div_izq.width, div_izq.height);
+  pintar_cosa_izq();
 
+  metY += dy;
 
-
-
-function draw_izq(){
-    ctx.clearRect(0, 0, div_izq.width, div_izq.height);
-    pintar_cosa_izq();
-    
-
-    metY += dy;
-
-    if(metY+150<0){
-        metY=800
-
-        
-    }
-    
-    
+  if (metY + 150 < 0) {
+    metY = 800;
+  }
 }
 
 setInterval(draw_izq, 10);
