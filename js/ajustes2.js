a09ca6eafd54baed224b6835aac96d8d32fa3bb1
amargura-evil-hehehe <100429271@alumnos.uc3m.es>
2022-04-06 18:37:49 +0200

    a

--- js/ajustes2.js
+++ js/ajustes2.js
@@ -14,5 +14,33 @@ function init(){
 	    ctx2.drawImage(nave1, 40, 20, 200, 150); 
 
     }); 
+    nave2.addEventListener("click",function(){
+        var nave2 = document.getElementById("imagen6"); 
+	    ctx2.drawImage(nave2, 40, 20, 200, 150); 
+
+    }); 
+    nave3.addEventListener("click",function(){
+        var nave3 = document.getElementById("imagen5"); 
+	    ctx2.drawImage(nave3, 40, 20, 200, 150); 
+
+    }); 
+    nave4.addEventListener("click",function(){
+        var nave4 = document.getElementById("imagen5"); 
+	    ctx2.drawImage(nave4, 40, 20, 200, 150); 
+
+    }); 
 
 }
+
+
+//esto es del query de lo de hide and show para enseñar y oculta cosas s
+//segun preferencia, usarlo tmbn para el icono de voulmen
+/*
+$(document).ready(function(){
+    $("#hide").click(function(){
+      $("p").hide();
+    });
+    $("#show").click(function(){
+      $("p").show();
+    });
+  });*/
