1d06a3cbe7f9ff924b1cc9f2d74336c13e4d9f2e
amargura-evil-hehehe <100429271@alumnos.uc3m.es>
2022-04-06 18:11:20 +0200

    a

--- js/ajustes2.js
+++ js/ajustes2.js
@@ -1,8 +1,18 @@
 var canvas2 = document.getElementById("personalizacion");
 var ctx2 = canvas2.getContext("2d");
 
+const nave1= document.getElementById("nave1");
+const nave2= document.getElementById("nave2");
+const nave3= document.getElementById("nave3");
+const nave4= document.getElementById("nave4");
+
 window.onload=init;
 
 function init(){ 
-    
+    nave1.addEventListener("click",function(){
+        var nave1 = document.getElementById("imagen5"); 
+	    ctx2.drawImage(nave1, 20, 30, 100, 100); 
+
+    }); 
+
 }
