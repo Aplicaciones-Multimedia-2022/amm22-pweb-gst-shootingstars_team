7809b446d9be983c7a510b71cbf16a3ddba882dc
amargura-evil-hehehe <100429271@alumnos.uc3m.es>
2022-04-07 17:35:51 +0200

    a

--- js/pruebaNave.js
+++
@@ -1,9 +0,0 @@
-var canvas2 = document.getElementById("personalizacion");
-var ctx2 = canvas2.getContext("2d");
-
-
-$(document).ready(function(){
-    $("#nave1").click(function(){
-    $  ("#imagen5").show();
-    });
-});
