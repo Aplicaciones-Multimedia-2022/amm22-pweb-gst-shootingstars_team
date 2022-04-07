f92c594cc483af11a2db381eb68625ca40556a70
amargura-evil-hehehe <100429271@alumnos.uc3m.es>
2022-04-07 14:16:18 +0200

    a

--- js/ajustes.js
+++ js/ajustes.js
@@ -25,10 +25,10 @@ imagen2.src = "../images/vol1.png";*/
 var canvas2 = document.getElementById("personalizacion");
 var ctx2 = canvas2.getContext("2d");
 
-const nave1= document.getElementById("nave1");
+/*const nave1= document.getElementById("nave1");
 const nave2= document.getElementById("nave2");
 const nave3= document.getElementById("nave3");
-const nave4= document.getElementById("nave4");
+const nave4= document.getElementById("nave4");*/
 
 
 
@@ -80,11 +80,30 @@ function init(){
 
    //para la parte de personalizacion
 
-    nave1.addEventListener("click",function(){
+   /* nave1.addEventListener("click",function(){
       var nave1 = document.getElementById("imagen5"); 
     ctx2.drawImage(nave1, 40, 20, 200, 150); 
 
-   }); 
+   }); */
+   
+   $(document).ready(function(){
+   $("#nave1").click(function(){
+   $  ("#imagen5").show();
+   });
+  });
+  /*
+  $(document).ready(function(){
+  $('#btn1').on("click",function(e){
+   $('#myImg').toggle('slow');
+  });
+});
+*/
+/*$(document).ready(function(){
+$('#nave1').on("click",function(){
+  $('#imagen5').toggle('slow');
+ });
+});*/
+
    nave2.addEventListener("click",function(){
       var nave2 = document.getElementById("imagen6"); 
     ctx2.drawImage(nave2, 40, 20, 200, 150); 
--- js/prejuego.js
+++ js/prejuego.js
@@ -1,16 +1,55 @@
+//grogus
 var mycanvas = document.getElementById("div_izq");
 var ctx = mycanvas.getContext("2d");
+var mycanvas2 = document.getElementById("div_drch");
+var ctx2 = mycanvas2.getContext("2d");
+
 
 var Meteorito1 = new Image();
+var Meteorito2 = new Image();
+
 
 var metX_izq = 0;
 var metY = 1400;
 var dy = -2;
 
+//ventana emergente
+var btn_abrir_easy = document.getElementById("btn");
+var overlay = document.getElementById("overlay");
+var ventana_easy = document.getElementById("ventana_easy");
+var btn_cerrar = document.getElementById("btn_cerrar");
+
+//botones
+const boton1= document.getElementById("boton1");
+const boton2= document.getElementById("boton2");
+const boton3= document.getElementById("boton3");
+const boton4= document.getElementById("boton4");
+const boton5= document.getElementById("boton5");
+const boton6= document.getElementById("bajustes");
+const boton8= document.getElementById("boton8");
+const botonFacil = document.getElementById("botonFacil");
+const botonMedio = document.getElementById("botonMedio");
+const botonDificil = document.getElementById("botonDificil");
+let sonido1 = new Audio();
+let espera;
+sonido1.src="../audio/click/39562__the-bizniss__mouse-click.wav";
+
+
+
+
+
+
+
+//grogus
 function pintar_cosa_izq() {
   Meteorito1.src = "../images/grogu.png";
   ctx.drawImage(Meteorito1, metX_izq, metY);
 }
+function pintar_cosa_drch() {
+  Meteorito2.src = "../images/grogu.png";
+  ctx2.drawImage(Meteorito2, metX_izq, metY);
+}
+
 
 function draw_izq() {
   ctx.clearRect(0, 0, div_izq.width, div_izq.height);
@@ -22,5 +61,105 @@ function draw_izq() {
     metY = 1400;
   }
 }
+function draw_dcha() {
+  ctx2.clearRect(0, 0, div_drch.width, div_drch.height);
+  pintar_cosa_drch();
+
+  metY += dy;
+
+  if (metY + 150 < 0) {
+    metY = 1400;
+  }
+}
+
+
+//ventana emergente
+btn_abrir_easy.addEventListener("click", function () {
+  overlay.classList.add("active");
+});
+
+btn_cerrar.addEventListener("click", function () {
+  overlay.classList.remove("active");
+});
+
+
+
+//botones
+boton1.addEventListener("click",function uno(){
+  sonido1.play();
+  espera = setTimeout(redireccionar1, 400);
+});
+boton2.addEventListener("click",function dos(){
+sonido1.play();
+espera = setTimeout(redireccionar2, 400);
+});
+boton3.addEventListener('click',function tres(){
+sonido1.play();
+espera = setTimeout(redireccionar3, 400);
+});
+boton4.addEventListener('click',function cuatro(){
+sonido1.play();
+espera = setTimeout(redireccionar4, 400);
+});
+boton5.addEventListener('click',function cinco(){
+sonido1.play();
+espera = setTimeout(redireccionar5, 400);
+});
+boton6.addEventListener('click',function seis(){
+sonido1.play();
+espera = setTimeout(redireccionar6, 400);
+});
+boton8.addEventListener('click',function ocho(){
+sonido1.play();
+espera = setTimeout(redireccionar8, 400);
+});
+botonFacil.addEventListener('click',function facil(){
+sonido1.play();
+espera = setTimeout(redireccionarFacil, 400);
+});
+botonMedio.addEventListener('click',function medio(){
+sonido1.play();
+espera = setTimeout(redireccionarMedio, 400);
+});
+botonDificil.addEventListener('click',function dificil(){
+sonido1.play();
+espera = setTimeout(redireccionarDificil, 400);
+});
+
+
+
+function redireccionar1(){
+  location.replace("../html/ComoJugar.html")
+}
+function redireccionar2(){
+  location.replace("../html/autores.html")
+}
+function redireccionar3(){
+  location.replace("../html/multimedia.html")
+}
+function redireccionar4(){
+  location.replace("../html/desarrollo.html")
+}
+function redireccionar5(){
+  location.replace("../html/referencias.html")
+}
+function redireccionar6(){
+  location.replace("../html/ajustes.html")
+}
+function redireccionar8(){
+  location.replace("../html/portada.html")
+}
+function redireccionarFacil(){
+  location.replace("../html/juego.html")
+}
+function redireccionarMedio(){
+  location.replace("../html/juego.html")
+}
+function redireccionarDificil(){
+  location.replace("../html/juego.html")
+}
+
+
 
 setInterval(draw_izq, 10);
+setInterval(draw_dcha, 10);
