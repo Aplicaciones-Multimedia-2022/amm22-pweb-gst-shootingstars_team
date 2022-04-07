4a9bf182b0984cbd32f3ab600336699042871c15
amargura-evil-hehehe <100429271@alumnos.uc3m.es>
2022-04-07 14:19:13 +0200

    a

--- js/multimedia.js
+++ js/multimedia.js
@@ -1,6 +1,6 @@
 
+const boton1= document.getElementById("boton1");
 const boton2= document.getElementById("boton2");
-const boton3= document.getElementById("boton3");
 const boton4= document.getElementById("boton4");
 const boton5= document.getElementById("boton5");
 const boton6= document.getElementById("bajustes");
@@ -13,13 +13,13 @@ sonido1.src="../audio/click/39562__the-bizniss__mouse-click.wav";
 window.onload=init;
 
 function init(){ 
+    boton1.addEventListener('click',function uno(){
+     sonido1.play();
+     espera = setTimeout(redireccionar1, 400);
+    }); 
     boton2.addEventListener("click",function dos(){
       sonido1.play();
       espera = setTimeout(redireccionar2, 400);
-    }); 
-    boton3.addEventListener('click',function tres(){
-      sonido1.play();
-      espera = setTimeout(redireccionar3, 400);
     }); 
     boton4.addEventListener('click',function cuatro(){
       sonido1.play();
@@ -45,12 +45,11 @@ function init(){
 }
 
 
-
+function redireccionar1(){
+  location.replace("../html/portada.html")
+}
 function redireccionar2(){
   location.replace("../html/autores.html")
-}
-function redireccionar3(){
-  location.replace("../html/multimedia.html")
 }
 function redireccionar4(){
   location.replace("../html/desarrollo.html")
