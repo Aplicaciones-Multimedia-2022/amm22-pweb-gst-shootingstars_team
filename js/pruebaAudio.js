16699f0ede510c77612de13bb2d45906cb8d541f
amargura-evil-hehehe <100429271@alumnos.uc3m.es>
2022-04-14 18:49:40 +0200

    a

--- js/pruebaAudio.js
+++ js/pruebaAudio.js
@@ -1,32 +1,31 @@
-$("#volume").slider({
-  min: 0,
-  max: 100,
-  value: 0,
-  range: "min",
-  animate: true,
-  slide: function(event, ui) {
-    setVolume((ui.value) / 100);
-  }
+
+const botones = document.getElementById('boton1');
+const controles = document.getElementById('unorderedList');
+
+var ruta;
+let sonido2= new Audio();
+sonido2.src="../audio/cancion1.mp3";
+
+botones.addEventListener('mouseenter', e => {
+  sonido2.play();
 });
 
-var myMedia = document.createElement('audio');
-$('#player').append(myMedia);
-myMedia.id = "myMedia";
-playAudio('http://iviewsource.com/exercises/audioslider/audio/ViewSource', 0);
+// mouseTarget.addEventListener('mouseleave', e => {
+//   mouseTarget.style.border = '1px solid #333';
+//   leaveEventCount++;
+//   addListItem('This is mouseleave event ' + leaveEventCount + '.');
+// });
 
-function playAudio(fileName, myVolume) {
-var mediaExt = (myMedia.canPlayType('audio/mp3')) ? '.mp3' 
-  : (myMedia.canPlayType('audio/ogg')) ? '.ogg' 
-  : '';
-if (mediaExt) {
-  myMedia.src = fileName + mediaExt;
-  myMedia.setAttribute('loop', 'loop');
-  setVolume(myVolume);
-  myMedia.play();
-}
-}
+// function addListItem(text) {
+//   // Create a new text node using the supplied text
+//   var newTextNode = document.createTextNode(text);
 
-function setVolume(myVolume) {
-  var myMedia = document.getElementById('myMedia');
-  myMedia.volume = myVolume;
-}
+//   // Create a new li element
+//   var newListItem = document.createElement("li");
+
+//   // Add the text node to the li element
+//   newListItem.appendChild(newTextNode);
+
+//   // Add the newly created list item to list
+//   unorderedList.appendChild(newListItem);
+// }
