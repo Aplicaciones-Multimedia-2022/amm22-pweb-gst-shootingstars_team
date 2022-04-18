a3758d38c3891933b958df00c9df56bd79c346e3
amargura-evil-hehehe <100429271@alumnos.uc3m.es>
2022-04-18 20:03:44 +0200

    a

--- js/prejuego.js
+++ js/prejuego.js
@@ -1,25 +1,54 @@
-//grogus
+//////////////////////////////////////////
+//                                      //
+//         CANVAS DE BABY YODA          //
+//                                      //
+//////////////////////////////////////////
+
+// CANVAS LONGITUDINALES EN LAS QUE APARECE EL AVATAR DE BABY YODA PARA DARLE DINAMISMO A LA PAGINA CON LA TEMATICA SEGUIDA
+
 var mycanvas = document.getElementById("div_izq");
 var ctx = mycanvas.getContext("2d");
 var mycanvas2 = document.getElementById("div_drch");
 var ctx2 = mycanvas2.getContext("2d");
+var Grogu1 = new Image(); // IMAGEN DE BABY YODA PARA EL CANVAS IZQ
+var Grogu2 = new Image(); // IMAGEN DE BABY YODA PARA EL CANVAS DRCHA
+var metX_izq = 0; // COORDENADA X DE BABY YODA PARA EL CANVAS IZQ
+var metY = 1400; // COORDENADA X DE BABY YODA PARA EL CANVAS DRCHA
+var dy = -2; // VELOCIDAD DE LOS BABY YODA
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-var Meteorito1 = new Image();
-var Meteorito2 = new Image();
+//////////////////////////////////////////
+//                                      //
+//       VENTANA EMERGENTE INFO         //
+//                                      //
+//////////////////////////////////////////
 
+// VENTANA EMERGENTE QUE TE INDICA INFORMACION DE LOS NIVELES
 
-var metX_izq = 0;
-var metY = 1400;
-var dy = -2;
-
-//ventana emergente
-var btn_abrir_easy = document.getElementById("btn");
+var btn_abrir_easy = document.getElementById("btn"); // REFERENCIAMOS EN UNA VARIABLE EL BOTON ABRIR LA VENTANA EMERGENTE
 var overlay = document.getElementById("overlay");
 var ventana_easy = document.getElementById("ventana_easy");
-var btn_cerrar = document.getElementById("btn_cerrar");
+var btn_cerrar = document.getElementById("btn_cerrar"); // REFERENCIAMOS EN UNA VARIABLE EL BOTON CERRAR LA VENTANA EMERGENTE
 
-//botones
+
+btn_abrir_easy.addEventListener("click", function () { // FUNCIONALIDAD DEL BOTON PARA ABRIR LA VENTANA EMERGENTE
+  overlay.classList.add("active");
+});
+
+btn_cerrar.addEventListener("click", function () { // FUNCIONALIDAD DEL BOTON PARA CERRAR LA VENTANA EMERGENTE
+  overlay.classList.remove("active");
+});
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////
+//                                      //
+//               BOTONES                //
+//                                      //
+//////////////////////////////////////////
+
 const boton1= document.getElementById("boton1");
 const boton2= document.getElementById("boton2");
 const boton3= document.getElementById("boton3");
@@ -32,59 +61,8 @@ const botonMedio = document.getElementById("botonMedio");
 const botonDificil = document.getElementById("botonDificil");
 let sonido1 = new Audio();
 let espera;
-sonido1.src="../audio/click/39562__the-bizniss__mouse-click.wav";
+sonido1.src="../audio/click/39562_the-bizniss_mouse-click.wav";
 
-
-
-
-
-
-
-//grogus
-function pintar_cosa_izq() {
-  Meteorito1.src = "../images/grogu.png";
-  ctx.drawImage(Meteorito1, metX_izq, metY);
-}
-function pintar_cosa_drch() {
-  Meteorito2.src = "../images/grogu.png";
-  ctx2.drawImage(Meteorito2, metX_izq, metY);
-}
-
-
-function draw_izq() {
-  ctx.clearRect(0, 0, div_izq.width, div_izq.height);
-  pintar_cosa_izq();
-
-  metY += dy;
-
-  if (metY + 150 < 0) {
-    metY = 1400;
-  }
-}
-function draw_dcha() {
-  ctx2.clearRect(0, 0, div_drch.width, div_drch.height);
-  pintar_cosa_drch();
-
-  metY += dy;
-
-  if (metY + 150 < 0) {
-    metY = 1400;
-  }
-}
-
-
-//ventana emergente
-btn_abrir_easy.addEventListener("click", function () {
-  overlay.classList.add("active");
-});
-
-btn_cerrar.addEventListener("click", function () {
-  overlay.classList.remove("active");
-});
-
-
-
-//botones
 boton1.addEventListener("click",function uno(){
   sonido1.play();
   espera = setTimeout(redireccionar1, 400);
@@ -127,6 +105,54 @@ espera = setTimeout(redireccionarDificil, 400);
 });
 
 
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////
+//                                      //
+//     PINTAR LOS BABY YODA             //
+//                                      //
+//////////////////////////////////////////
+
+// GENERAMOS LAS IMAGENES QUE QUEREMOS REPRESENTAR
+function pintar_cosa_izq() { 
+  Grogu1.src = "../images/grogu.png";
+  ctx.drawImage(Grogu1, metX_izq, metY);
+}
+function pintar_cosa_drch() {
+  Grogu2.src = "../images/grogu.png";
+  ctx2.drawImage(Grogu2, metX_izq, metY);
+}
+
+// CON LAS FUNCIONES DRAW DIBUJAMOS EL MOVIMIENTOS DE NUESTROS BABY YODAS
+function draw_izq() {
+  ctx.clearRect(0, 0, div_izq.width, div_izq.height);
+  pintar_cosa_izq();
+
+  metY += dy;
+
+  if (metY + 150 < 0) {
+    metY = 1400;
+  }
+}
+function draw_dcha() {
+  ctx2.clearRect(0, 0, div_drch.width, div_drch.height);
+  pintar_cosa_drch();
+
+  metY += dy;
+
+  if (metY + 150 < 0) {
+    metY = 1400;
+  }
+}
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////
+//                                      //
+//    FUNCIONES PARA REDIRECIONAR       //
+//                                      //
+//////////////////////////////////////////
 
 function redireccionar1(){
   location.replace("../html/ComoJugar.html")
@@ -162,9 +188,17 @@ function redireccionarDificil(){
   localStorage.setItem('dificultad', '3');
 }
 
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////
+//                                      //
+//   FUNCIONES PARA GUARDAR EL NOMBRE   //
+//                                      //
+//////////////////////////////////////////
+
 function sub(){
-  var n = document.getElementById("nombreuser").value;
-  localStorage.setItem('nombre' , n);
+  var n = document.getElementById("nombreuser").value; // GUARDAMOS EN LA VARIABLE N EL NOMBRE QUE HEMOS INTRODUCIDO EN EL FORMULARIO
+  localStorage.setItem('nombre' , n); // UTILIZAMOS LOCAL STORAGE PARA PODER GUARDAR LA VARIABLE Y UTILIZARLA EN OTRO JS
 }
 
 
